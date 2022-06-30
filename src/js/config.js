ba669a9694a2b87c15c77bb2b3ee20cf805736d3
hu hu <huardian3154@gmail.com>
2022-06-30 20:46:28 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'db865851-8bb4-4e59-b98e-3d152737dbdb';
+export const MODAL_CLOSE_SEC = 2.5;
